ce81cf54e4b8a55898a7905eae5604bf067a00af
Gusta Marius <gustamarius@gmail.com>
2021-06-03 08:48:00 +0300

    interfata grafica

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/ServerThread.java
@@ -0,0 +1,8 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import java.net.ServerSocket;
+
+public class ServerThread extends Thread{
+    private int port = 0;
+    private ServerSocket serverSocket = null;
+}
